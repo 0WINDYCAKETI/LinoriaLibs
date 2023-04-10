3bb56d3de859eaabfa5f68c76979464027585446
0Windycaketi <112848663+0WINDYCAKETI@users.noreply.github.com>
2023-04-10 12:50:33 +0700

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
 local SaveManager = {} do
-	SaveManager.Folder = 'Azuner'
+	SaveManager.Folder = 'AzunerSetting'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
